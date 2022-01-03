25f5b973eba7b3bcb0c13f5ff7bff37e08518e74
nhan2k <nhanrooney113@gmail.com>
2022-01-04 04:40:11 +0700

    Add Router & Controller

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+// XMLHttpRequest, getch, axios
 
 // HTTP logger
 app.use(morgan("combined"));
@@ -32,15 +42,8 @@ app.engine(
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-console.log(path.join(__dirname, "resources/views"));
 // Route
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
