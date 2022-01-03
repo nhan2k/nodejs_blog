9d6b705cb8e897a742b06f438e88a7bc538c76c3
nhan2k <nhanrooney113@gmail.com>
2022-01-04 06:09:38 +0700

    Install prettierm lint-staged, husky

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,13 @@
 class SiteController {
-  // [GET] /site
-  index(req, res) {
-    res.render("home");
-  }
+    // [GET] /site
+    index(req, res) {
+        res.render('home');
+    }
 
-  //   [GET] /search
-  search(req, res) {
-    res.render("search");
-  }
+    //   [GET] /search
+    search(req, res) {
+        res.render('search');
+    }
 }
 
 module.exports = new SiteController();
